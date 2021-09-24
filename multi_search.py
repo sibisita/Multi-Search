7ac0f2235c3a954a9ba24f6a34ce77afc3026406
Sibi <sitasibi@gmail.com>
2021-09-24 14:50:12 +0530

    Version 3

---
+++ multi_search.py
@@ -0,0 +1,219 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import scrolledtext
+import os,time
+from datetime import datetime
+from tkinter import messagebox
+from getpass import getuser
+
+root=Tk()
+
+about_this_application='''
+
+Hi,
+
+This application is created for searching a list of IPs from hundreds of network device backup folder. This can be be used for other use cases as well.
+
+Source code for this is available in : https://github.com/sibisita/Multi-Search/blob/main/multi-search.py
+
+
+Sibi
+
+'''
+
+def new_window():
+    window11 = Toplevel(root)
+    window11.title("About This Application!")
+    a11=Text(window11)
+    a11.insert(INSERT,about_this_application)
+    a11.pack()
+
+Current_working_directory = ("/".join(os.getcwd().split("\\")))
+search_location1 = Current_working_directory
+save_location1 = Current_working_directory
+chvar=IntVar()
+chvar.set(0)#checkbox variable
+number_of_files_to_search=0
+
+def logs_entry(log):
+    statusbar.configure(state='normal')
+    statusbar.insert(END, log+"\n")
+    statusbar.configure(state='disabled')
+    statusbar.yview(END)
+    root.update_idletasks()
+
+def search_one_file(search_value_list,file_path,save_file_handler,local_counter):
+    data=[]
+    try:
+        with open(file_path,"r") as f1:
+            data=f1.readlines()
+        #time.sleep(0.05)# rest of 50ms
+        logs_entry("Now reading : \n"+file_path)
+    except:
+        logs_entry("\n\nError while reading : \n"+file_path+"\n\n")
+        return local_counter
+
+    first_find={} #To include the file name for the first find of each values
+    
+    for value in search_value_list:
+        first_find[value]=True
+        
+    index=0   
+    
+    for line in data:
+        for value in search_value_list:
+            if value in line.lower():
+            
+                if first_find[value]:
+                    save_file_handler[value].write("\n\n*****"+file_path+"*****\n")
+                    first_find[value]=False
+                save_file_handler[value].write(str(index)+" : "+line)
+                local_counter[value]+=1
+        index+=1
+    #time.sleep(0.05)# rest of 50ms
+    return local_counter
+
+
+
+def file_path_func():
+    files_all=[]
+    if chvar.get() == 0: 
+        files_all = [ (search_location1+"/"+f) for f in os.listdir(search_location1) if os.path.isfile(os.path.join(search_location1, f))]
+    else:  
+        for (root_dir,_,files) in os.walk(search_location1, topdown=True):
+            for i in files:
+                files_all.append(root_dir+"/"+i)
+    return files_all
+
+def search_in_folder():
+    temp_var = filedialog.askdirectory()
+    if temp_var != "":
+        global search_location1
+        search_location1 = temp_var
+        l1.configure(text=search_location1)
+
+def save_in_folder():
+    temp_var = filedialog.askdirectory()
+    if temp_var != "":
+        global save_location1 
+        save_location1 = temp_var
+        l2.configure(text=save_location1)
+
+
+def search_main_func():
+    search.configure(state="disabled")
+    start_time=datetime.now() #Start time to calculate time taken
+
+    # Creating folder to save result
+    i=1
+    global save_location1
+    multi_search_folder=save_location1+str("/Multi-Search")
+    lenght_multi_search_folder=len(multi_search_folder)
+    while(os.path.exists(multi_search_folder)):
+        multi_search_folder=multi_search_folder[:lenght_multi_search_folder]+str(i)
+        i+=1
+    os.mkdir(multi_search_folder)
+    
+    # Extracting values from screen
+    search_values=(text_area.get("1.0","end")).lower().splitlines()
+    for x in search_values:
+        if len(x)==0:
+            search_values.remove(x) #removing empty entries.  
+    
+    #initiating counter for each value
+    counter={}
+    for x in search_values:
+        counter[x]=0
+
+    file_path_list = file_path_func() 
+    global number_of_files_to_search #for getting an idea of how much time is required
+    number_of_files_to_search = len(file_path_list)
+
+    completed_files_count=0
+    f1={} #To handle multiple files
+    for value in search_values:
+        f1[value]=open(multi_search_folder+"/"+value+".txt","a+")
+    for i in file_path_list:
+        counter = search_one_file (search_values,i,f1,counter)
+        completed_files_count+=1
+        file_count.configure(text="Search progress stats : {} / {} Completed".format(completed_files_count,number_of_files_to_search))
+        root.update_idletasks()
+    for value in search_values:
+        f1[value].close()
+        #logic to delete empty files from save folder
+        if counter[value]==0:
+            os.remove(multi_search_folder+"/"+value+".txt")
+            logs_entry("deleted Empty file : "+multi_search_folder+"/"+value+".txt")
+
+    
+
+    end_time=datetime.now() #End time of the whole search operation.
+    completed_in=(end_time-start_time).total_seconds() #time taken calculated in seconds
+
+    logs_entry("Search of {} files completed in {} seconds.\n\n".format(completed_files_count,completed_in))
+
+    with open(multi_search_folder+"/logfile.log","w+") as f1:
+        f1.write("Search of {} files completed in {} seconds.\n\n".format(completed_files_count,completed_in))
+        global search_location1
+        f1.write("Search conducted by '"+getuser()+"' at "+search_location1+"\n\n")
+        f1.write(metadata_area.get("1.0","end"))
+        f1.write("\n\nSearched Values\t:\tCount\n")
+        f1.write("\n"+str(counter).replace(", ","\n").replace(":","\t\t:\t")[1:-1])
+
+    MsgBox = messagebox.askquestion ('Exit Application','Search Completed. Press Yes to exit.\n Press no to verify logs. ',icon = 'info')
+    if MsgBox == 'yes':
+        root.destroy()
+    else:
+        output_window.grid(column=11,row=1,pady=5, padx=5, rowspan=100,sticky="nw")
+        output_window.configure(state='normal')
+        output_window.insert(END, "Search of {} files completed in {} seconds.\n\n".format(completed_files_count,completed_in))
+        output_window.insert(END, "\n\nSearched Values  \t:\tCount\n")
+        output_window.insert(END, str(counter).replace(", ","\n").replace(":","\t\t:\t")[1:-1])
+        output_window.configure(state='disabled')
+        output_window.yview(END)
+        output_lable.grid(column=11, row=0,sticky="w",columnspan=2)
+    
+
+
+
+#front end
+root.title("Multi-Search")
+ttk.Label(root, text="Enter the values to be searched below: ",font=("Times New Roman", 15)).grid(column=0, row=0,sticky="w",columnspan=2)
+text_area = scrolledtext.ScrolledText(root, wrap=WORD, width=30, height=15,font=("Times New Roman", 12))
+search=ttk.Button(root,text="Search",command=search_main_func)
+about=ttk.Button(root,text="About",width=8,command=new_window)
+metadata_area = scrolledtext.ScrolledText(root, wrap=WORD, width=25, height=10,font=("Times New Roman", 12))
+metadata_area.configure(state='normal')
+metadata_area.insert(END, "Metadata : \n\n")
+subdir=ttk.Checkbutton(root,text="Use subdirectory",variable=chvar)
+l1=ttk.Label(root, text=search_location1)
+search_loc=ttk.Button(root,text="Browse search folder",command=search_in_folder)
+l2=ttk.Label(root, text=save_location1)
+save_loc=ttk.Button(root,text="Browse Save folder",command=save_in_folder)
+space=ttk.Label(root, text=" ")
+file_count=ttk.Label(root, text="Search progress stats will appear here!!!",font=("Times New Roman", 14))
+statusbar=scrolledtext.ScrolledText(root, wrap=WORD, width=60, height=15,font=("Times New Roman", 12))
+statusbar.insert(INSERT, "Here comes the logs\n\n")
+statusbar.configure(state='disabled')
+output_window=scrolledtext.ScrolledText(root, wrap=WORD, width=30, height=30,state='disabled',font=("Times New Roman", 12))
+output_lable=ttk.Label(root, text="Searched values and their number of occurances: ",font=("Times New Roman", 10))
+photo = PhotoImage(file = ".\\icon.png")
+root.iconphoto(False,photo)
+
+#grid position
+text_area.grid(column=0, row=2, pady=1, padx=1,sticky="w",rowspan=15,columnspan=2)
+search.grid(column=3, row=6)
+about.grid(column=4, row=0,padx=5)
+metadata_area.grid(column=3, row=8, padx=1,sticky="w")
+subdir.grid(column=3, row=5)
+l1.grid(column=0, row=18, pady=1, padx=1,sticky="w")
+search_loc.grid(column=3, row=18, padx=1,sticky="w")
+space.grid(column=0, row=20, pady=1, padx=1,sticky="w")
+l2.grid(column=0, row=21, pady=1, padx=1,sticky="w")
+save_loc.grid(column=3, row=21, padx=1,sticky="w")
+file_count.grid(column=0,row=25,pady=10, padx=10)
+statusbar.grid(column=0,row=27,pady=5, padx=5,columnspan=10,sticky=W)
+
+text_area.focus()
+root.mainloop()
